02ce8e5111694f0b47e5eea8e5f34560ad9870e0
Le_Roux-nard <65075968+Le-Roux-nard@users.noreply.github.com>
2024-07-17 16:39:21 +0200

    update twitter regex to ignore fixupx.com

--- src/classes/regexs/twitter.ts
+++ src/classes/regexs/twitter.ts
@@ -1,7 +1,7 @@
 import Matcher from "../Matcher";
 
 export default class TwitterMatcher implements Matcher {
-	testRegex = /(?<=\bhttps?:\/\/(?:www\.)?)twitter|x(?=\.com\/.+?\/status\/\d+\b)/g;
+	testRegex = /(?<=\bhttps?:\/\/(?:www\.)?)twitter|(?<=[^A-Za-z-0-9])x(?=\.com\/.+?\/status\/\d+\b)/g;
 
 	exec(str: string): string {
 		return str.replace(this.testRegex, "fxtwitter");
