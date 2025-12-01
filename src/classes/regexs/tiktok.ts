6a5736ce4b5f518c5da5ff62da84d19dcf0a79c4
Le-Roux-Nard <lerouxnard@lerouxnard.fr>
2025-12-01 12:46:58 +0100

    switch to kkscript for tiktok

--- src/classes/regexs/tiktok.ts
+++ src/classes/regexs/tiktok.ts
@@ -4,6 +4,6 @@ export default class TikTokMatcher implements Matcher {
 	testRegex = /vm\.tiktok\.com(?=\/.+?\b\/)|(?:www\.)?tiktok\.com(?=\/@[A-Za-z_0-9]+\/video\/\d+)/g;
 
 	exec(str: string): string {
-		return str.replace(this.testRegex, "vm.vxtiktok.com");
+		return str.replace(this.testRegex, "kktiktok.com");
 	}
 }
